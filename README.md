5986aae77c5fa7a772788a94c227b8a195150112
bagas <bagassambega@gmail.com>
2024-05-01 13:59:37 +0700

    feat: first initialization using dictionary API

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Tucil3_13522071
-Tugas Kecil 3 Strategi Algoritma: Word Ladder Solver
+# Tugas Kecil 3 Strategi Algoritma
+## Word Ladder Solver
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Main {
+import java.io.IOException;
+import java.net.*;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        Scanner input = new Scanner(System.in);
+        String first = input.nextLine();
+        String second = input.nextLine();
+
+        URL start = new URI("https://api.dictionaryapi.dev/api/v2/entries/en/" + first).toURL();
+        HttpURLConnection conn = (HttpURLConnection) start.openConnection();
+        conn.setRequestMethod("GET");
+        conn.connect();
+        int responseCode = conn.getResponseCode();
+
+        if (responseCode != 200) {
+            System.out.println("Error: " + responseCode);
+            return;
+        } else {
+            Scanner sc = new Scanner(start.openStream());
+            StringBuilder inline = new StringBuilder();
+            while (sc.hasNext()) {
+                inline.append(sc.nextLine());
+            }
+            sc.close();
+            System.out.println(inline);
+        }
+    }
 }
