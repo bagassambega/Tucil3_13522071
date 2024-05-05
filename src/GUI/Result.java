18db7a466c74efdce3f696bb8350e54fc0eb9f87
bagas <bagassambega@gmail.com>
2024-05-05 13:02:25 +0700

    fix: layout GUI and add bin

--- src/GUI/Result.java
+++ src/GUI/Result.java
@@ -35,7 +35,8 @@ public class Result extends JFrame {
 
         if (pathResult == null || pathResult.isEmpty()) {
             JLabel label = new JLabel("Tidak ada jalur yang ditemukan.", SwingConstants.CENTER);
-            label.setFont(label.getFont().deriveFont(16.0f));
+            label.setPreferredSize(new Dimension(500, 80));
+            label.setFont(new Font("Calibri", Font.BOLD, 18));
             mainPanel.add(label);
         } else {
             JPanel resultPanel = new JPanel();
@@ -46,26 +47,29 @@ public class Result extends JFrame {
                 resultPanel.add(label);
             }
             JScrollPane scrollPane = new JScrollPane(resultPanel);
-            scrollPane.setPreferredSize(new Dimension(400, 180));
+            scrollPane.setPreferredSize(new Dimension(450, 180));
             scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
             scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
             scrollPane.setVisible(true);
             mainPanel.add(scrollPane);
+            JLabel length = new JLabel("Kata dilalui: " + (pathResult.size() - 2));
+            mainPanel.add(length);
+
         }
 
         // Tampilkan jumlah kata yang diperiksa
         JLabel numberOfWords = new JLabel("", SwingConstants.CENTER);
         JLabel memoryUsage = new JLabel("", SwingConstants.CENTER);
         if (method == 0) {
-            numberOfWords.setText("Banyak artikel diperiksa: " + UCS.checkedNode);
+            numberOfWords.setText("Banyak kata diperiksa: " + UCS.checkedNode);
             memoryUsage.setText("Memory usage: " + UCS.memoryUsage/1024 + " KB");
             UCS.checkedNode = 0;
         } else if (method == 1) {
-            numberOfWords.setText("Banyak artikel diperiksa: " + GBFS.checkedNode);
+            numberOfWords.setText("Banyak kata diperiksa: " + GBFS.checkedNode);
             memoryUsage.setText("Memory usage: " + GBFS.memoryUsage/1024 + " KB");
             GBFS.checkedNode = 0;
         } else {
-            numberOfWords.setText("Banyak artikel diperiksa: " + A_Star.checkedNode);
+            numberOfWords.setText("Banyak kata diperiksa: " + A_Star.checkedNode);
             memoryUsage.setText("Memory usage: " + A_Star.memoryUsage/1024 + " KB");
             A_Star.checkedNode = 0;
         }
